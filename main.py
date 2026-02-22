db588e4849148d2b637237623b7891dc2c886090
joss413 <Yosephn22@gmail.com>
2026-02-22 12:43:08 +0800

    added feature to input name

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def print_hi(name):
 
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Yoseph Negash')
+    name=input("Enter your name:")
+    print(f'Hello, {name}!')
+
 
 
