110f337a4c092e4d14385597cc2e1a35204b0546
joss413 <Yosephn22@gmail.com>
2026-02-22 14:26:09 +0800

    change in feature-2

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def print_hi(name):
 
 if __name__ == '__main__':
    # some additional feature for feature-1 branch
+   # this line should cause a conflict
    # some new changes in main
     name=input('Enter your Full name')
     print_hi(name)
